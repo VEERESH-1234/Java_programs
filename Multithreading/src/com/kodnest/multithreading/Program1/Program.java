2041f7de5f2b829b828d42c1123e166cc93f6b16
Veeresh Shirasangi <118192848+VEERESH-1234@users.noreply.github.com>
2024-02-15 23:00:03 +0530

    Add files via upload

---
+++ Multithreading/src/com/kodnest/multithreading/Program1/Program.java
@@ -0,0 +1,7 @@
+package com.kodnest.multithreading.Program1;
+
+public class Program extends Thread
+{
+	Thread t=Thread.currentThread();
+	System.out.println(t);
+}
---
+++ Multithreading/src/com/kodnest/multithreading/Program1/ProgramApp.java
@@ -0,0 +1,10 @@
+package com.kodnest.multithreading.Program1;
+
+public class ProgramApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
