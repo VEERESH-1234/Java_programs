f0b13ebd8836719ee5db86bf962cf345bcdf6384
Veeresh Shirasangi <118192848+VEERESH-1234@users.noreply.github.com>
2024-02-15 22:59:08 +0530

    Add files via upload

---
+++ MethodsPractice/src/Program.java
@@ -0,0 +1,16 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Program pr=new Program();
+		int x=10;
+		pr.display(x);
+
+	}
+	public void display(int x)
+	{
+		System.out.println("the value of x:"+x);
+	}
+
+}
