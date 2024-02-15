c4ae05fc35084b84b5d6418898aedf38314f38de
Veeresh Shirasangi <118192848+VEERESH-1234@users.noreply.github.com>
2024-02-15 22:00:51 +0530

    Add files via upload

---
+++ If Statment Control Constructs/src/TechClubApp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TechClubApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the marks");
+		int marks=scan.nextInt();
+		if(marks>90)
+		{
+			System.out.println("Welcome To TechClub");
+		}
+
+	}
+
+}
